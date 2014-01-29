437feb330198c1a496e1b823d371e585f0df66bb
Mikkel Markvardsen <mikkelmarkvardsen@gmail.com>
2014-01-29 16:16:23 +0100

    BetAppWebser commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+bets = require('./routes/bets');
+ 
+var app = express();
+ 
+app.configure(function () {
+  app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+  app.use(express.bodyParser());
+});
+ 
+app.get('/bets', bets.findAll);
+app.get('/bets/:id', bets.findById);
+app.post('/bets', bets.addBet);
+app.put('/bets/:id', bets.updateBet);
+app.delete('/bets/:id', bets.deleteBet);
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');
