e266f0a8caf3de3a54e4b9c4e9fe8a2a2a8a30b6
Mikkel Markvardsen <mikkelmarkvardsen@gmail.com>
2014-01-31 15:50:02 +0100

    small caps db name

--- routes/bets.js
+++ routes/bets.js
@@ -5,7 +5,7 @@ var Server = mongo.Server,
     BSON = mongo.BSONPure;
  
 var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('Betsdb', server);
+db = new Db('betsdb', server);
  
 db.open(function(err, db) {
     if(!err) {
