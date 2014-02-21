7830a8ea8c26f89ea89a474b1c92944812e0a7a0
jonaslading <j_lading@hotmail.com>
2014-02-21 13:25:50 +0100

    allowing CORS

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 app.configure(function () {
   app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
   app.use(express.bodyParser());
+  app.use(allowCrossDomain);
 });
  
 app.get('/bets', bets.findAll);
